4d46b6ce260fa4005ae713faa4c9cc486286e8cc
uttne <uttne.mkr@gmail.com>
2021-09-08 22:27:10 +0900

    init

---
+++ src/GitVersionSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitVersionSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
